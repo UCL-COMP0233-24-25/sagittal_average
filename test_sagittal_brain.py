b9196880776fa2d278cb15b6d9d1ddc20e037b68
am1520x <am1520@ic.ac.uk>
2024-11-11 11:52:32 +0000

    added a test with expected v actual output arrays.

--- test_sagittal_brain.py
+++ test_sagittal_brain.py
@@ -1,5 +1,16 @@
 import numpy as np
+import pytest
+from sagittal_brain import run_averages
 
 data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
 np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+#changed the axis from 0 to 1, the previous code was looing at the wrong plane, hence the non-zero averages
+
+data_output = np.zeros((20,1))
+data_output[-1, :] = 1.0
+np.savetxt("brain_average_output.csv", data_output, fmt='%d', delimiter=',')
+expected_result = np.loadtxt("brain_average_output.csv", delimiter=',')
+run_averages("brain_sample.csv", "brain_average.csv")
+actual_result = np.loadtxt("brain_average.csv", delimiter=',')
+assert  np.all(actual_result) == np.all(expected_result), "Files are not equal"
