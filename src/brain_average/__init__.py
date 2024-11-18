c7bc10b33d08b3408c53ed1753d7b326afa43f7a
Chayapon Thunsetkul <chayapon.thunsetkul.24@ucl.ac.uk>
2024-11-18 11:21:18 +0000

    added tests

---
+++ src/brain_average/__init__.py
@@ -0,0 +1,1 @@
+from brain_average.sagittal_brain import *
--- tests/test_sagittal_brain.py
+++ tests/test_sagittal_brain.py
@@ -1,24 +1,28 @@
 import numpy as np
-from src.brain_average.sagittal_brain import run_averages
 import subprocess
 
-test_input = np.zeros((20, 20))
-test_input[-1, :] = 1.
+from brain_average import run_averages
 
-expected = np.zeros(20)
-expected[-1] = 1.
+def test_average_brain():
+    test_input = np.zeros((20, 20))
+    test_input[-1, :] = 1.
 
-# Save input to brain_sample.csv
-np.savetxt("brain_sample.csv", test_input, fmt='%d', delimiter=',')
+    expected = np.zeros(20)
+    expected[-1] = 1.
+
+    # Save input to brain_sample.csv
+    np.savetxt("brain_sample.csv", test_input, fmt='%d', delimiter=',')
 
 
-# run sagital_brain.py
-subprocess.run(["python", "sagittal_brain.py", "brain_sample.csv", "brain_average.csv"])
+    # run sagital_brain.py
+    run_averages(file_input="brain_sample.csv",
+                 file_output="brain_average.csv")
 
 
-# Read output from brain_average.csv
-output = np.loadtxt("brain_average.csv", delimiter=',')
 
-# Compare the outpare with the expected output
-assert np.all(output) == np.all(expected)
+    # Read output from brain_average.csv
+    output = np.loadtxt("brain_average.csv", delimiter=',')
+
+    # Compare the outpare with the expected output
+    assert np.all(output) == np.all(expected)
 
