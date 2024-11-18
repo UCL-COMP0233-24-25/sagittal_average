d8db0297fa497a2b880662b8c6f58f8b65ee7dba
am1520x <am1520@ic.ac.uk>
2024-11-18 12:31:11 +0000

    Updating everything according to issue 32 so that tests work.

--- tests/test_call_command.py
+++ tests/test_call_command.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run(["python", "../src/sagittal_brain/sagittal_brain.py"], shell=True)
+subprocess.run(["python", "src/sagittal_brain/sagittal_brain.py"], shell=True)
--- tests/test_sagittal_brain.py
+++ tests/test_sagittal_brain.py
@@ -1,19 +1,22 @@
-import numpy as np
+
 import pytest
+import numpy as np
 import subprocess
-from src.sagittal_brain.sagittal_brain import run_averages
+from sagittal_brain.sagittal_brain import run_averages
 
 data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
-np.savetxt("../src/sagittal_brain/brain_sample.csv", data_input, fmt='%d', delimiter=',')
+np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
 #changed the axis from 0 to 1, the previous code was looing at the wrong plane, hence the non-zero averages
 
 
-subprocess.run(["python", "../src/sagittal_brain/sagittal_brain.py"], shell=True)
+subprocess.run(["python", "src/sagittal_brain/sagittal_brain.py"], shell=True)
 data_output = np.zeros((20,1))
 data_output[-1, :] = 1.0
 np.savetxt("brain_average_output.csv", data_output, fmt='%d', delimiter=',')
-expected_result = np.loadtxt("brain_average_output.csv", delimiter=',')
-run_averages("../src/sagittal_brain/brain_sample.csv", "../src/sagittal_brain/brain_average.csv")
-actual_result = np.loadtxt("../src/sagittal_brain/brain_average.csv", delimiter=',')
-np.testing.assert_array_equal(actual_result, expected_result)
+
+def test_function(file_path_expected = "brain_average_output.csv", file_path_actual = "brain_average.csv"):        
+    expected_result = np.loadtxt(file_path_expected, delimiter=',')
+    run_averages("brain_sample.csv", "brain_average.csv")
+    actual_result = np.loadtxt(file_path_actual, delimiter=',')
+    assert np.all(actual_result) == np.all(expected_result), "The results dont match the expected"
