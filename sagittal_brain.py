dc5663fb8e9654642b82bfdca934cadc1c16612b
am1520x <am1520@ic.ac.uk>
2024-11-18 10:45:01 +0000

    Added a doc string and restructured the repo to make it inline with an installable package.

--- sagittal_brain.py
+++ sagittal_brain.py
@@ -6,10 +6,23 @@ import numpy as np
 def run_averages(file_input='brain_sample.csv', file_output='brain_average.csv'):
     """
     Calculates the average through the coronal planes
-    The input file should has as many columns as coronal planes
-    The rows are intersections of the sagittal/horizontal planes
+    The input file should has as many columns as coronal planes.
+    The rows are intersections of the sagittal/horizontal planes.
 
-    The result is the average for each sagittal/horizontal plane (rows)
+    The result is the average for each sagittal/horizontal plane (rows).
+    Parameters
+    ----------
+    file_input (str): 
+        The file path for the input data, the collected sample.
+    file_output (str):
+        The file path for the output data, the calculated average.
+    Returns
+    -------
+    file
+        saves the output data to the file path provided in the file_output parameter.
+    Example
+    -------
+    >>>run_averages(file_input, file_output)
     """
     # Open the file to analyse
     planes = np.loadtxt(file_input, dtype=int,  delimiter=',')
--- tests/test_sagittal_brain.py
+++ tests/test_sagittal_brain.py
@@ -5,15 +5,15 @@ from sagittal_brain import run_averages
 
 data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
-np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+np.savetxt("../brain_sample.csv", data_input, fmt='%d', delimiter=',')
 #changed the axis from 0 to 1, the previous code was looing at the wrong plane, hence the non-zero averages
 
 
-subprocess.run(["python", "sagittal_brain.py"], shell=True)
+subprocess.run(["python", "../sagittal_brain.py"], shell=True)
 data_output = np.zeros((20,1))
 data_output[-1, :] = 1.0
 np.savetxt("brain_average_output.csv", data_output, fmt='%d', delimiter=',')
 expected_result = np.loadtxt("brain_average_output.csv", delimiter=',')
-run_averages("brain_sample.csv", "brain_average.csv")
-actual_result = np.loadtxt("brain_average.csv", delimiter=',')
+run_averages("../brain_sample.csv", "../brain_average.csv")
+actual_result = np.loadtxt("../brain_average.csv", delimiter=',')
 assert  np.all(actual_result) == np.all(expected_result), "Files are not equal"
